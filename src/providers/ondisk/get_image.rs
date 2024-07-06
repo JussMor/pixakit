d61909bb9a91fc147baf7780995b5df4942f9c68
Junior Moreira <jussmor@outlook.es>
2024-07-06 15:19:14 -0500

    resize image on the fly

--- src/providers/ondisk/get_image.rs
+++ src/providers/ondisk/get_image.rs
@@ -2,19 +2,27 @@ use ntex::web::{self, get, HttpResponse,  Error};
 use ntex::web::types::{Path, Query, State};
 use std::fs;
 use std::path::PathBuf;
+use std::sync::Once;
 use serde::Deserialize;
+use magick_rust::{MagickWand, magick_wand_genesis};
+
 
 use crate::AppState;
 
 #[derive(Debug,Deserialize)]
 struct ImageProcessing {
-    width: Option<u32>,
-    height: Option<u32>,
+    width: Option<usize>,
+    height: Option<usize>,
 }
 
+static START: Once = Once::new();
 
 #[get("/images/{tail}*")]
 async fn get_image(state: State<AppState>, tail: Path<String>, params: Query<ImageProcessing>) ->  Result<HttpResponse, Error>  {
+
+    START.call_once(|| {
+        magick_wand_genesis();
+    });
 
     let tail_path = tail.into_inner();
     let width = params.width;
@@ -49,15 +57,39 @@ async fn get_image(state: State<AppState>, tail: Path<String>, params: Query<Ima
     let full_path = PathBuf::from("ondisk_storage").join(&tail_path);
 
     if full_path.exists() && full_path.is_file() {
+        let mut wand = MagickWand::new();
         let mime_type = mime_guess::from_path(&full_path).first_or_octet_stream();
         let file_content = fs::read(&full_path).map_err(|e| {
             web::error::ErrorInternalServerError(format!("Failed to read file: {}", e))
         })?;
 
-        // Store the processed image in the cache
-        state.cache.insert(base_url.clone(), file_content.clone()).await;
+        if let Err(err) = wand.read_image_blob(&file_content) {
+            eprintln!("Failed to read image: {:?}", err);
+            return Ok(HttpResponse::InternalServerError().finish());
+        }
 
-        Ok(HttpResponse::Ok().content_type(mime_type.to_string()).body(file_content))
+        if let (Some(w), Some(h)) = (width, height) {
+            if let Err(err) = wand.resize_image(w, h, magick_rust::FilterType::Lanczos) {
+                eprintln!("Failed to resize image: {:?}", err);
+                return Ok(HttpResponse::InternalServerError().finish());
+            }
+        }
+        // Convert the image to WEBP format
+        wand.set_image_format("WEBP").map_err(|e| {
+            eprintln!("Failed to set image format: {:?}", e);
+            web::error::ErrorInternalServerError("Failed to set image format")
+        })?;
+
+        // Get the processed image data
+        let processed_image_data = wand.write_image_blob("WEBP").map_err(|e| {
+            eprintln!("Failed to write image blob: {:?}", e);
+            web::error::ErrorInternalServerError("Failed to write image blob")
+        })?;
+
+        // Store the processed image in the cache
+        state.cache.insert(base_url.clone(), processed_image_data.clone()).await;
+
+        Ok(HttpResponse::Ok().content_type(mime_type.to_string()).body(processed_image_data))
     } else {
         Err(web::error::ErrorNotFound("File not found").into())
     }
