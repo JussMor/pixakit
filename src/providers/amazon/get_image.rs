b11bdffac30b8e6a6287762a31c75f77e14b8903
Junior Moreira <jussmor@outlook.es>
2024-07-10 00:58:13 -0500

    feat: setting amazon service

--- src/providers/amazon/get_image.rs
+++ src/providers/amazon/get_image.rs
@@ -1,16 +1,14 @@
-use futures_util::StreamExt;
-use google_cloud_storage::http::objects::download::Range;
-use google_cloud_storage::http::objects::get::GetObjectRequest;
 use ntex::web::{ self, get, HttpResponse, Error};
 use ntex::web::types::{Path, State, Query};
 use serde::Deserialize;
 use magick_rust::{MagickWand, magick_wand_genesis};
 use std::sync::Once;
-
-
-
-use crate::AppState;
 use ntex_bytes::BytesMut;
+
+
+use crate::providers::amazon::S3Error;
+use crate::AppState;
+
 
 #[derive(Debug,Deserialize)]
 struct PathParams {
@@ -31,17 +29,94 @@ static START: Once = Once::new();
 #[get("/images/{container}/{name}")]
 async fn get_images(state: State<AppState>, path: Path<PathParams>, params: Query<ImageProcessing>) -> Result<HttpResponse, Error> {
 
-  let object_from_aws = state.aws_client.list_objects_v2().prefix("").bucket("pixakit".to_string());
+    START.call_once(|| {
+        magick_wand_genesis();
+    });
 
-    let req = object_from_aws.send().await;
+    let object_from_aws = state.aws_client
+        .get_object()
+        .bucket(&path.container)
+        .key(&path.name)
+        .send()
+        .await;
 
-    let keys = req
-            .iter()
-            .filter_map(|o| o.key_count.as_ref())
-            .map(|s| s.to_string())
-            .collect::<Vec<_>>();
+    let width = params.width;
+    let height = params.height;
 
-    println!("key {:?}", keys);
+    let mut base_url = format!("/images/{}/{}", &path.container, &path.name);
 
-  Ok(HttpResponse::Ok().body("File uploaded successfully"))
+    let mut query_params = vec![];
+    if let Some(w) = width {
+        query_params.push(format!("width={}", w));
+    }
+    if let Some(h) = height {
+        query_params.push(format!("height={}", h));
+    }
+
+    // Append query parameters to the base URL if they exist
+    if !query_params.is_empty() {
+        base_url.push('?');
+        base_url.push_str(&query_params.join("&"));
+    }
+
+    // Check if the processed image is in the cache
+    if let Some(cached_image) = state.cache.get(&base_url).await {
+        println!("getting from cache");
+        return Ok(HttpResponse::Ok().content_type("image/webp").body(cached_image));
+    }
+
+    match object_from_aws {
+        Ok(output) => {
+            let mut body = BytesMut::new();
+            let mut stream = output.body;
+
+            while let Some(item) = stream.next().await {
+                match item {
+                    Ok(response) => {
+                        body.extend_from_slice(&response);
+                    },
+                    Err(err) => {
+                        eprintln!("Error reading object stream from S3: {:?}", err);
+                        return Err(Error::from(S3Error(err)));
+                    }
+                }
+            }
+
+            let mut wand = MagickWand::new();
+            let file_content = body.to_vec();
+
+            if let Err(err) = wand.read_image_blob(&file_content) {
+                eprintln!("Failed to read image: {:?}", err);
+                return Ok(HttpResponse::InternalServerError().finish());
+            }
+
+            if let (Some(w), Some(h)) = (width, height) {
+                if let Err(err) = wand.resize_image(w, h, magick_rust::FilterType::Lanczos) {
+                    eprintln!("Failed to resize image: {:?}", err);
+                    return Ok(HttpResponse::InternalServerError().finish());
+                }
+            }
+
+            // Convert the image to WEBP format
+            wand.set_image_format("WEBP").map_err(|e| {
+                eprintln!("Failed to set image format: {:?}", e);
+                web::error::ErrorInternalServerError("Failed to set image format")
+            })?;
+
+            // Get the processed image data
+            let processed_image_data = wand.write_image_blob("WEBP").map_err(|e| {
+                eprintln!("Failed to write image blob: {:?}", e);
+                web::error::ErrorInternalServerError("Failed to write image blob")
+            })?;
+
+            // Store the processed image in the cache
+            state.cache.insert(base_url.clone(), processed_image_data.clone()).await;
+
+            Ok(HttpResponse::Ok().content_type("image/webp").body(processed_image_data))
+        }
+        Err(err) => {
+            eprintln!("Error fetching object from S3: {:?}", err);
+            Ok(HttpResponse::InternalServerError().body("Failed to fetch image"))
+        }
+    }
 }
--- src/providers/amazon/mod.rs
+++ src/providers/amazon/mod.rs
@@ -1,2 +1,16 @@
+use aws_sdk_s3::primitives::ByteStreamError;
+use ntex::web::WebResponseError;
 pub mod router;
 pub mod get_image;
+
+
+#[derive(Debug)]
+struct S3Error(ByteStreamError);
+
+impl WebResponseError for S3Error {}
+
+impl std::fmt::Display for S3Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+}
